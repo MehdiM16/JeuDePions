1a49b0546ff682fa4923bfdbff06da53c8564ce5
immoune <g.immoune94@gmail.com>
2021-04-27 22:52:01 +0200

    botJoue

--- view/Partie.java
+++ view/Partie.java
@@ -9,6 +9,7 @@ import java.awt.event.*;
 import java.awt.image.*;
 import java.io.*;
 import javax.imageio.*;
+import java.util.*;
 
 
 
@@ -45,6 +46,7 @@ public class Partie {
     		System.out.println("bonjour");
     		System.out.println(game.p.toString());
     		remplissage_grille();
+    		bot_joue();
     		panel.add(grille, BorderLayout.CENTER);
     		frame.setContentPane(panel);
     		
@@ -88,7 +90,8 @@ public class Partie {
     			if(tab[i][j].getEtat() == ' ') cellule.setBackground(new Color(255,255,255));
     			else if(tab[i][j].getEtat() == 'X') cellule.setBackground(new Color(255,0,0));
     			else if(tab[i][j].getEtat() == 'O') cellule.setBackground(new Color(0,0,255));
-    			cellule.addActionListener((event) -> {System.out.println("le chouch"); game.tourdejeu(a,b); draw();});
+    			if (game.aquiletour && !victoire()) cellule.addActionListener((event) -> {System.out.println("le chouch"); game.tourdejeu(a,b); draw();});
+    			else cellule.setEnabled(false);
     			boutton[i][j] = cellule;
     			
     		}
@@ -113,9 +116,121 @@ public class Partie {
     
     
     public void bot_joue() {
-    	
+    	boolean endGame = false;
+    	//boolean start = true;
+    	int maxX = -1;
+		int maxY = -1;
+		int winner = 0;
+    	Random r = new Random();
+    	if(!game.aquiletour && !victoire()) {
+	    	if (game.p.sameScore()) {
+	    		System.out.println("je suis le numero 1");
+				game.p.initNbKuplets(game.k);
+			    XY res = game.p.getCaseMieuxPlacee();
+			    botTour.addActionListener((event) -> {game.tourOrdinateur(res.x, res.y); draw();});
+			    //jeu.tourOrdinateur(res.x, res.y);
+			    //start = false;
+			}
+			else {
+			    ArrayList<XY> maxEvals = game.getMaxEvals();
+			    ArrayList<XY> bestEvals = game.getBestEvals(maxEvals); 
+			    int rand = r.nextInt(bestEvals.size());
+			    XY choice = bestEvals.get(rand);	
+			    //System.out.println("L'ordinateur a joué sur ("+choice.x+","+" "+choice.y+")");
+			    //jeu.tourOrdinateur(choice.x, choice.y);
+			    botTour.addActionListener((event) -> {
+			    	game.tourOrdinateur(choice.x, choice.y);
+			    	System.out.println("L'ordinateur a joué sur ("+choice.x+","+" "+choice.y+")"); 
+			    	draw();
+			    });
+			}
+    	}
+    	else {
+    		botTour.setEnabled(false);
+    	}
+    	int highEval = game.getHighEval();
     }
     
+    
+    public boolean victoire() {
+    	System.out.println("nous sommes au coeur du probleme");
+    	NewDriver driv = new NewDriver();
+    	int[] res = driv.MAJscores(game.k);
+    	int maxX = res[0];
+    	int maxY = res[1];
+    	int winner = -1;
+    	int highEval = game.getHighEval();
+    	if (highEval==maxX) {
+			JLabel label = new JLabel("Vous avez gagné félicitation");
+			panel.add(label, BorderLayout.SOUTH);
+			return true;
+		} else if (highEval==maxY) {
+			JLabel label = new JLabel("Vous avez perdu");
+			panel.add(label, BorderLayout.SOUTH);
+			return true;
+		}
+		if (game.pasDeCaseVide()) {
+			JLabel label = new JLabel("La partie est nulle");
+			panel.add(label, BorderLayout.SOUTH);
+			return true;
+		}
+		return false;
+    }
+    
+    
+    
+    
+    
+    /*public void deroulement() {
+    	draw();
+    	boolean endGame = false;
+    	boolean start = true;
+    	int maxX = -1;
+		int maxY = -1;
+		int winner = 0;
+    	Random r = new Random();
+		while (!endGame) {
+			if (game.aquiletour) {
+				//int xy[] = jeu.promptXY(sc);
+				//System.out.println("Vous avez joué sur ("+xy[0]+","+" "+xy[1]+")");
+				//jeu.tourdejeu(xy[0], xy[1]);
+				remplissage_grille();
+			}
+			else {	
+				if (game.p.sameScore() || start) {
+					game.p.initNbKuplets(game.k);
+				    XY res = game.p.getCaseMieuxPlacee();
+				    botTour.addActionListener((event) -> {game.tourOrdinateur(res.x, res.y); draw();});
+				    //jeu.tourOrdinateur(res.x, res.y);
+				    start = false;
+				}
+				else {
+				    ArrayList<XY> maxEvals = game.getMaxEvals();
+				    ArrayList<XY> bestEvals = game.getBestEvals(maxEvals); 
+				    int rand = r.nextInt(bestEvals.size());
+				    XY choice = bestEvals.get(rand);	
+				    System.out.println("L'ordinateur a joué sur ("+choice.x+","+" "+choice.y+")");
+				    //jeu.tourOrdinateur(choice.x, choice.y);
+				    botTour.addActionListener((event) -> {game.tourOrdinateur(choice.x, choice.y); draw();});
+				}
+			}
+			int highEval = game.getHighEval();
+			if (highEval==maxX) {
+				winner = 1;
+				endGame = true;
+			} else if (highEval==maxY) {
+				winner = 0;	
+				endGame = true;
+			}
+			if (!endGame && game.pasDeCaseVide()) {
+				winner = 2;			
+				endGame = true;
+			}
+			System.out.println(game.p.toString());	
+		}
+		//messageVainqueur(winner);
+    }
+    */
     
     public static void main(String[] args) {
     	Partie p = new Partie(new Jeu(10,10,4));
